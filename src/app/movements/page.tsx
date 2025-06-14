dd40429432e2f0cc0b0bd2422b4832f41f7f910a
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2025-06-14 10:18:26 -0300

    feat: add advanced movements

--- src/app/movements/page.tsx
+++ src/app/movements/page.tsx
@@ -1,16 +1,14 @@
 import { Cube } from "@/components/Cube";
-import { CubeCase, movimentosNotations } from "@/types";
+import { CubeCase, advancedMovements, basicMovements } from "@/types";
 
 export default function Movements() {
   return (
     <>
-      <h1 className="text-3xl">Movimentos Básicos</h1>
+      <h1 className="text-3xl">Movimentos</h1>
       <p className="py-8">
         Para solucionar o Cubo Mágico nós vamos utilizar algumas fórmulas, que
         são sequencias de movimentos que podem ser escritos (traduzidos) com
         letras. Parece muito complexo, mas é extremamente simples e intuitivo.
-      </p>
-      <p className="py-4">
         Cada lado do cubo é representado pela sua inicial em inglês. Se a letra
         estiver sozinha significa que o movimento é no sentido horário. Se
         estiver acompanhada de um apóstrofo, significa que é no sentido
@@ -18,8 +16,23 @@ export default function Movements() {
         duplo.
       </p>
 
+      <h2 className="text-2xl text-center py-8">Movimentos Básicos</h2>
+
       <div className="flex flex-wrap gap-y-8 gap-x-24 justify-center">
-        {movimentosNotations.map((notation: CubeCase) => (
+        {basicMovements.map((notation: CubeCase) => (
+          <Cube
+            key={notation.name}
+            name={notation.name}
+            colors={notation.colors}
+            description={notation.description}
+          />
+        ))}
+      </div>
+
+      <h2 className="text-2xl text-center py-8">Movimentos Avançados</h2>
+
+      <div className="flex flex-wrap gap-y-8 gap-x-24 justify-center">
+        {advancedMovements.map((notation: CubeCase) => (
           <Cube
             key={notation.name}
             name={notation.name}
--- src/types/movements.ts
+++ src/types/movements.ts
@@ -1,6 +1,6 @@
 import { CubeCase } from "./firstTwoLayers";
 
-export const movimentosNotations: CubeCase[] = [
+export const basicMovements: CubeCase[] = [
   {
     name: "R - Direita Horário",
     colors: [
@@ -90,5 +90,81 @@ export const movimentosNotations: CubeCase[] = [
       ["r", "r", "r", "y", "y", "y", "y", "y", "y"],
     ],
     description: "Movimento da face traseira no sentido horário",
+  },
+];
+
+export const advancedMovements: CubeCase[] = [
+  {
+    name: "Uw' - Topo Duplo Anti-horário",
+    colors: [
+      ["o", "o", "b", "o", "o", "b", "o", "o", "b"],
+      ["b", "b", "b", "b", "b", "b", "r", "r", "r"],
+      ["y", "y", "y", "y", "y", "y", "y", "y", "y"],
+    ],
+    description: "Movimento de duas camadas do topo no sentido anti-horário",
+  },
+  {
+    name: "Fw - Frente Duplo Horário",
+    colors: [
+      ["b", "b", "b", "b", "b", "b", "b", "b", "b"],
+      ["y", "y", "r", "y", "y", "r", "y", "y", "r"],
+      ["y", "y", "y", "o", "o", "o", "o", "o", "o"],
+    ],
+    description: "Movimento de duas camadas da face no sentido horário",
+  },
+  {
+    name: "Lw' - Esquerda Duplo Anti-horário",
+    colors: [
+      ["b", "b", "b", "w", "w", "w", "w", "w", "w"],
+      ["r", "r", "r", "r", "r", "r", "r", "r", "r"],
+      ["b", "b", "y", "b", "b", "y", "b", "b", "y"],
+    ],
+    description:
+      "Movimento de duas camadas da esquerda no sentido anti-horário",
+  },
+  {
+    name: "M - Meio",
+    colors: [
+      ["b", "b", "b", "y", "y", "y", "b", "b", "b"],
+      ["r", "r", "r", "r", "r", "r", "r", "r", "r"],
+      ["y", "g", "y", "y", "g", "y", "y", "g", "y"],
+    ],
+    description: "Movimento da camada do meio no mesmo sentido do L",
+  },
+  {
+    name: "S - Meio",
+    colors: [
+      ["b", "b", "b", "b", "b", "b", "b", "b", "b"],
+      ["r", "y", "r", "r", "y", "r", "r", "y", "r"],
+      ["y", "y", "y", "o", "o", "o", "y", "y", "y"],
+    ],
+    description: "Movimento da camada do meio no mesmo sentido do F",
+  },
+  {
+    name: "X - Cubo Todo",
+    colors: [
+      ["w", "w", "w", "w", "w", "w", "w", "w", "w"],
+      ["r", "r", "r", "r", "r", "r", "r", "r", "r"],
+      ["b", "b", "b", "b", "b", "b", "b", "b", "b"],
+    ],
+    description: "Movimento do cubo todo no mesmo sentido do R",
+  },
+  {
+    name: "Y - Cubo Todo",
+    colors: [
+      ["r", "r", "r", "r", "r", "r", "r", "r", "r"],
+      ["g", "g", "g", "g", "g", "g", "g", "g", "g"],
+      ["y", "y", "y", "y", "y", "y", "y", "y", "y"],
+    ],
+    description: "Movimento do cubo todo no mesmo sentido do U",
+  },
+  {
+    name: "Z - Cubo Todo",
+    colors: [
+      ["b", "b", "b", "b", "b", "b", "b", "b", "b"],
+      ["y", "y", "y", "y", "y", "y", "y", "y", "y"],
+      ["o", "o", "o", "o", "o", "o", "o", "o", "o"],
+    ],
+    description: "Movimento do cubo todo no mesmo sentido do F",
   },
 ];
